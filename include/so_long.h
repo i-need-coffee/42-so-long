135da5a1aacc586282db66922db20423afb5e95d
Shadya Jolliet <shadya.jolliet@outlook.com>
2026-02-08 21:00:44 +0100

    start to build the program and understand minilibx

--- include/so_long.h
+++ include/so_long.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sjolliet <sjolliet@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sjolliet <sjolliet@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 11:40:57 by sjolliet          #+#    #+#             */
-/*   Updated: 2026/02/06 14:22:53 by sjolliet         ###   ########.fr       */
+/*   Updated: 2026/02/08 20:56:04 by sjolliet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,16 @@
 
 # include "libft.h"
 # include "mlx.h"
+# include <X11/keysym.h>
+# include <X11/X.h>
+
+# define WIN_WIDTH 500
+# define WIN_HEIGHT 500
+
+typedef struct s_mlx_data
+{
+	void	*connect;
+	void	*window;
+}			t_mlx_data;
 
 #endif
--- src/so_long.c
+++ src/so_long.c
@@ -3,21 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sjolliet <sjolliet@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sjolliet <sjolliet@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 11:40:06 by sjolliet          #+#    #+#             */
-/*   Updated: 2026/02/06 19:04:11 by sjolliet         ###   ########.fr       */
+/*   Updated: 2026/02/08 20:53:05 by sjolliet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+int	handle_input(int keysym, t_mlx_data *mlx)
+{
+	if (keysym == XK_Escape)
+	{
+		ft_printf("The %d key (ESC) has been pressed\n\n", keysym);
+		mlx_destroy_window(mlx->connect, mlx->window);
+		mlx_destroy_display(mlx->connect);
+		free(mlx->connect);
+		exit(1);
+	}
+	ft_printf("The %d key has been pressed\n\n", keysym);
+	return (0);
+}
+
 int	main(void)
 {
-	void	*mlx;
+	t_mlx_data	mlx;
 
-	mlx = mlx_init();
-	if (!mlx)
+	mlx.connect = mlx_init();
+	if (!mlx.connect)
 		return (1);
+	mlx.window = mlx_new_window(mlx.connect, WIN_WIDTH, WIN_HEIGHT, "so_long");
+	if (!mlx.window)
+	{
+		mlx_destroy_display(mlx.connect);
+		free(mlx.connect);
+		return (1);
+	}
+	mlx_key_hook(mlx.window, handle_input, &mlx);
+	mlx_loop(mlx.connect);
+	mlx_destroy_window(mlx.connect, mlx.window);
+	mlx_destroy_display(mlx.connect);
+	free(mlx.connect);
 	return (0);
 }
