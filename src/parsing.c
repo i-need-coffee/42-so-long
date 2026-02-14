4a22b04756d23000da0586ff6df475e2fa9cbb7e
Shadya Jolliet <shadya.jolliet@outlook.com>
2026-02-14 03:27:00 +0100

    join the file to check if the map is valid in parsing

--- src/parsing.c
+++ src/parsing.c
@@ -3,21 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sjolliet <sjolliet@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sjolliet <sjolliet@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 15:41:07 by sjolliet          #+#    #+#             */
-/*   Updated: 2026/02/13 17:51:43 by sjolliet         ###   ########.fr       */
+/*   Updated: 2026/02/14 03:18:46 by sjolliet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+static char	*get_joined_file(int fd);
+static int	check_chars(char *map);
+
 void	parse_map(char *file)
 {
 	char	*ext;
-	char	*line;
-	char	*join;
-	char	*tmp;
+	char	*joined_map;
 	int		fd;
 
 	ext = ft_strrchr(file, '.');
@@ -26,17 +27,56 @@ void	parse_map(char *file)
 	fd = open(file, O_RDONLY);
 	if (fd == -1)
 		error_and_exit("File could not be opened");
+	joined_map = get_joined_file(fd);
+	close(fd);
+	if (!joined_map)
+		error_and_exit("Problem while reading the file");
+	if (!check_chars(joined_map))
+	{
+		free(joined_map);
+		error_and_exit("There are invalid chars in the map");
+	}
+	free(joined_map);
+}
+
+static char	*get_joined_file(int fd)
+{
+	char	*line;
+	char	*temp;
+	char	*join;
+
 	line = get_next_line(fd);
 	join = NULL;
-	while (line != NULL)
+	while (line)
 	{
-		tmp = ft_strjoin(join, line);
+		temp = ft_strjoin(join, line);
+		if (!temp)
+		{
+			free(line);
+			free(join);
+			return (NULL);
+		}
 		free(join);
-		join = tmp;
+		join = temp;
 		free(line);
 		line = get_next_line(fd);
 	}
 	free(line);
-	free(join);
-	close(fd);
+	return (join);
+}
+
+static int	check_chars(char *map)
+{
+	char	*valid_chars;
+	int		i;
+
+	valid_chars = "01CEP\n";
+	i = 0;
+	while (map[i])
+	{
+		if (!ft_strrchr(valid_chars, map[i]))
+			return (0);
+		i++;
+	}
+	return (1);
 }
